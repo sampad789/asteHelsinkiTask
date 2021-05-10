6e541191e2c54c22dcbc815fd8e1a7ff88c23909
sampad789 <sampad789@gmail.com>
2021-05-10 13:29:06 +0300

    DEPLOyment error fixed

--- server.js
+++ server.js
@@ -54,15 +54,13 @@ app.get("/search", (req, res) => {
     });
 });
 
-// Serve static assests if in production eg, deploying to heroku
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// Serve static assests eg, deploying to heroku
+// Set static folder
+app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 // Define a port to run a server on or a deploy env port
 const PORT = process.env.PORT || 5000;
 

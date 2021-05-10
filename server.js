f4d937cb4f4e98eaeb69c0e5cf8f38f0c8330bfd
sampad789 <sampad789@gmail.com>
2021-05-10 13:03:14 +0300

    DEPLOyment error fixed

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get("/search", (req, res) => {
 // Serve static assests if in production eg, deploying to heroku
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));

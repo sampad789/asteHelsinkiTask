c6f7b1f1b3829f565232e2924476ffea1c618376
sampad789 <sampad789@gmail.com>
2021-05-31 17:06:58 +0300

    Fixes

--- server.js
+++ server.js
@@ -59,11 +59,11 @@ app.get("/search", (req, res) => {
 
 //
 if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 // Define a port to run a server on or a deploy env port
